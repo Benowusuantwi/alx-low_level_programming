8351b3ad650cd6b41a46bd00a8efaa65393ba816
Ben_X <benowusuantwi819@gmail.com>
2023-10-06 07:10:56 -0700

    Task 4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print out a line with puts
+ * Return :0 (betty should return 0 if code is successful)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
