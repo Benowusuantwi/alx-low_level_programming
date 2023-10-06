d7d310e8afa3bf3405da0a488b1a6fe6d0468d4d
Ben_X <benowusuantwi819@gmail.com>
2023-10-06 12:50:14 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - a program that prints a text to the standard error
+ * Return: 1 (success)
+*/
+int main(void)
+{
+	char string[] = "and that piece of art is useful\" -\
+			 Dora Korpar, 2015-10-19\n;
+	write(2, string, sizeof(string));
+	return (1);
+}
