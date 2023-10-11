be3867f9c076ca987abea8393bd95f757250e403
Ben_X <benowusuantwi819@gmail.com>
2023-10-11 09:37:28 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - a function to print lowercase alphabets 10x
+ * print_alphabet_x10 - a function to print lowercase alphabets 10x
  * Return: 0
  */
 void print_alphabet_x10(void)
