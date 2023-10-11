5cec2a447c887560dfb20aa55ba2793bb624ad6d
Ben_X <benowusuantwi819@gmail.com>
2023-10-11 09:18:07 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - a function to print lowercase alphabets 10x
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char x;
+	int numb_row;
+
+	for (numb_row = 0; numb_row < 10; numb_row++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+}
