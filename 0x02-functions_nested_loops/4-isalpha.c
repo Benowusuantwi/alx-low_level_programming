8d32a9bdf13c1ec7806314a8ebddb02cdfa0fdec
Ben_X <benowusuantwi819@gmail.com>
2023-10-11 14:10:04 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - a function to print lowercase alphabets
+ * @c: input of the function _isalpha
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
