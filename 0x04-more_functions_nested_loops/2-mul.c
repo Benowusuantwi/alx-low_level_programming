dd6dca025c6eae157249c873424589a174b30136
Ben_X <benowusuantwi819@gmail.com>
2023-10-13 14:44:25 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *mul - a function to print lowercase alphabets
+ *@a: input parameter
+ *@b: input parameter
+ *Return: result
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
