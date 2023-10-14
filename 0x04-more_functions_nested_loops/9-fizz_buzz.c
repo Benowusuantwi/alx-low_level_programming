0f6d9d4e80a3d449310ea8c83ff2c87b68e57b27
Ben_X <benowusuantwi819@gmail.com>
2023-10-14 12:08:51 -0700

    Task 10

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - print s numbers from 0-100
+ * but prints Fizz, Buzz and FizzBuzz if a number is
+ * divisible by 3, 5 and both 3 and 5 respectively
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("FzzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
