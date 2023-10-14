940af1f2e4fe6d205e330bba22eca49424444fe0
Ben_X <benowusuantwi819@gmail.com>
2023-10-14 08:10:48 -0700

    Task 5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,15 +6,15 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i = 0; i < 10; i++)
 	{
-		if (i != 50)
+		if (i != 2)
 		{
-			if (i != 52)
+			if (i != 4)
 			{
 				_putchar(i);
 			}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
