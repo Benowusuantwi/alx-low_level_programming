0f2a04f2f9ddc3a3e4ac436845d8bc9b6db6525c
Ben_X <benowusuantwi819@gmail.com>
2023-10-14 10:18:57 -0700

    Task 7

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+nclude "main.h"
+/**
+ * print_line - a function to print lowercase alphabets
+ * @n: input parameter
+ */
+
+void print_line(int n)
+{
+	for (n = 0; n <= i; n++)
+	{
+		if (n > 0)
+		{
+			_putchar(137);
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
