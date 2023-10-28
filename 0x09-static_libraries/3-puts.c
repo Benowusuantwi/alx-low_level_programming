4632bf6acb6ad7bad76de7c1cdd5302242204890
Ben_X <benowusuantwi819@gmail.com>
2023-10-27 17:45:22 -0700

    Done, finally

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <unistd.h>
+int _putchar(char c);
 /**
  * _puts - a function for the library
  * @s: input string
@@ -6,5 +8,21 @@
 
 void _puts(char *s)
 {
-	/* code is in 0x05*/
+	while (*s != '\0')
+	{
+		_putchar(*s++);
+	}
+	_putchar('\n');
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
